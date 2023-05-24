bf270e7fe82bf6a9c75873df36e33e4e61d7c8ec
suketti <54027485+suketti@users.noreply.github.com>
2023-05-24 10:53:43 +0200

    task 5

--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -4,15 +4,64 @@
     {
         static void Main(string[] args)
         {
-            List <Expression> expressions = new List <Expression> ();
+            List<Expression> expressions = new List<Expression>();
             StreamReader sr = new StreamReader("kifejezesek.txt", encoding: System.Text.Encoding.UTF8);
             while (!sr.EndOfStream)
             {
                 string[] lineIn = sr.ReadLine().Split(' ');
-                Console.WriteLine(lineIn[2]);
+                expressions.Add(new Expression(Int32.Parse(lineIn[0]), lineIn[1], Int32.Parse(lineIn[2])));
             }
 
-            sr.Close();
+            Console.WriteLine("2. feladat: Kifejezesések száma: " + expressions.Count);
+            Console.WriteLine("3. feladat: Kifejezések maradékos osztással" + expressions.Where(e => e.Operand == "mod").Count());
+            if (expressions.Where(e => e.FirstNum % 10 == 10 || e.SecondNum % 10 == 10).Count() > 0)
+            {
+                Console.WriteLine("4. feladat: Létezik ilyen kifezejés");
+            } 
+            else
+            {
+                Console.WriteLine("4. feladat: Nem létezik ilyen kifejezés");
+            }
+
+            int additionCount = 0;
+            int substractionCount = 0;
+            int multiplationCount = 0;
+            int divisionCount = 0;
+            int wholeDivisionDount = 0;
+            int remainderCount = 0;
+
+
+            foreach(Expression e in expressions)
+            {
+                switch(e.Operand)
+                {
+                    case "+":
+                        additionCount++;
+                        break;
+                    case "-":
+                        substractionCount++;
+                        break;
+                    case "*":
+                        multiplationCount++;
+                        break;
+                    case "/":
+                        divisionCount++;
+                        break;
+                    case "div":
+                        wholeDivisionDount++;
+                        break;
+                    case "mod":
+                        remainderCount++;
+                        break;
+                }
+            }
+
+            Console.WriteLine("5. feladat: Statisztika");
+            Console.WriteLine($"mod -> { remainderCount}db\n /-> {divisionCount}db\n div->{wholeDivisionDount}db\n - -> {substractionCount}db\n *-> {multiplationCount}db\n + -> {additionCount}db");
+
         }
+
+
+
     }
 }
