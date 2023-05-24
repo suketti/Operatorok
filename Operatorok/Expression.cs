8259723e341fc8fe19ab77f89ae54817b33168c8
suketti <54027485+suketti@users.noreply.github.com>
2023-05-24 10:32:55 +0200

    20 perc mire sikerult normalisan utf-8-ra fixelni a forrasfajlt

---
+++ Operatorok/Expression.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorok
+{
+    public class Expression
+    {
+        int firstNum;
+        string operand;
+        int secondNum;
+
+
+        public Expression(int firstNum, string operand, int secondNum)
+        {
+            this.firstNum = firstNum;
+            this.operand = operand;
+            this.secondNum = secondNum;
+        }
+
+        public int FirstNum { get => firstNum; set => firstNum = value; }
+        public string Operand { get => operand; set => operand = value; }
+        public int SecondNum { get => secondNum; set => secondNum = value; }
+    }
+}
--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List <Expression> expressions = new List <Expression> ();
+            StreamReader sr = new StreamReader("kifejezesek.txt", encoding: System.Text.Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                string[] lineIn = sr.ReadLine().Split(' ');
+                Console.WriteLine(lineIn[2]);
+            }
+
+            sr.Close();
         }
     }
 }
